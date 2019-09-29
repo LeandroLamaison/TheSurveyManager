7f23977252d0ee8bb8a6f6d24f2f2f4696a83363
LeandroLamaison <lamaisonleandro2@gmail.com>
2019-09-29 18:25:27 -0300

    Requisito 5 e comentários

--- src/thesurveymanager/Question.java
+++ src/thesurveymanager/Question.java
@@ -1,72 +1,130 @@
 package thesurveymanager;
 
+//O ArrayList é usado para registrar as questões e suas alternativas em uma lista
 import java.util.ArrayList;
+
+//O scanner é usado para receber a mensagem do usuário
 import java.util.Scanner;
 
+//Classe questão, contém os dados de cada questão, uma lista que as armazena
+// e metodos para exibi-las e responde-las 
 public class Question {
+    
+    //Lista que armazena as questões registradas
     public static  ArrayList<Question> list = new ArrayList<>();
     
+    //Identificador da questão
     public int id;
+    
+    //String contendo a pergunta
     public String message = new String();
+    
+    //Lista contendo as alternativas
     public ArrayList<Answer> answers = new ArrayList<>();
     
+    
+    //Construtor da questão
     Question(String message) {
+        //A mensagem ´da questão é passada pelo parâmetro
         this.message = message;
+        
+        //Quando a questão é criada, ela é automaticamente adicionada`à lista de questões
         Question.list.add(this);
+        
+        //O id da questão é sempre sua posição na lista
         this.id = Question.list.indexOf(this); 
     }
   
-    
+    //Método que mostra a questão
     public void ShowQuestion() {
+        //Exibe o id da questão
         System.out.println("     Question " + this.id);
+        
+        //Exibe a pergunta
         System.out.println(this.message);
+        
+        //Exibe cada uma das alternativas da lista
         this.answers.forEach(n -> System.out.println(n.message));
     }
     
     public void AnswerQuestion() {
+        //Criação de um scanner
         Scanner s = new Scanner(System.in);
         
+        //Chama o método que mostra a questão
         this.ShowQuestion();
         
+        //Solicita a resposta do usuário
         System.out.println("Digite a letra da resposta");
         String answer = s.nextLine();
         
-        if(!answer.equals("a") && !answer.equals("b") && !answer.equals("c") && !answer.equals("d") && !answer.equals("e")) {
+        //Enquanto a resposta não corresponder a nenhuma das alternativas,
+        //exibe uma mensagem de erro e solicita a resposta de novo
+        while(!answer.equals("a") && !answer.equals("b") && !answer.equals("c") && !answer.equals("d") && !answer.equals("e")) {
             System.out.println("Resposta invalida. Digite a letra da resposta");
             answer = s.nextLine();
         }
         
+        //Adiciona a resposta ao último histórico criado (o que foi adicionado quando o método ApplySurvey foi chamado)
+        History.list.get(History.list.size() - 1).add( Integer.toString(this.id), answer);
+        
+        //Checa todas as respostas
         for(int i = 0; i < this.answers.size() ; i++) {
+            
+            //Quando a resposta do usuário corresponder a uma das alternaivas
             if(this.answers.get(i).id.equals(answer)) {
+                
+                //Soma mais um ao contador da alternativa selecionada
                 this.answers.get(i).count += 1;
+                break;
             }
+            
         }
         
+        //Linha em branco
         System.out.println("");   
     }
     
-    
+    //Método que mostra os resultados da pesquisa (número de respostas por alternativas e porcentagem )
     public void getAnswers() {
+        //Exibe o id da questão
         System.out.println("     Question " + this.id);
+        
+        //Exibe a pergunta
         System.out.println(this.message);
+        
+        //Exibe cada uma das alternativas, juntamente com o 
+        //número de vezes que foram selecionadas e a porcentagem
         this.answers.forEach(answer -> {
             System.out.println(answer.message + " --- " + answer.count + " --- " + this.getPercent(answer));
         });
+        
+        //Linha em branco
         System.out.println("");
     }
     
+    
+    //Método que obtém a porcentagem, auxiliar ao método getAnswers
     public String getPercent(Answer answer) {
+        //Cria a variavel que representa o total de respostas
         float total = 0;
+        
+        //Soma os contadores de todas as alternativas da questão
         for (Answer ans: this.answers) {
             total += ans.count;
         }
         
+        //Cria o método que representa a porcentagem,
+        //através da fórmula da porcentagem
         float percent = (answer.count * 100) / total;
         
+        //Se a porcentagem não for um número, o que acontecerá caso o número de 
+        //respostas total seja zero, a porcentagem é setada como zero 
         if(Float.isNaN(percent)) {
             percent = 0;
         }
         
+        //retorna a procentagem 
         return new String(percent + "%");
     }
     
--- src/thesurveymanager/TheSurveyManager.java
+++ src/thesurveymanager/TheSurveyManager.java
@@ -1,13 +1,17 @@
 package thesurveymanager;
 
-import java.util.ArrayList;
+//O scanner é usado na interface para dar os comandos
 import java.util.Scanner;
 
+//Classe principal
 public class TheSurveyManager {
    
+    //Método principal
     public static void main(String[] args) {
+        //Criação de um scanner 
         Scanner s = new Scanner(System.in);
         
+        //Registro das perguntas
         Question question1 = new Question("Responda a primeira pergunta");
         question1.answers.add(new Answer("a","primeira altenativa"));
         question1.answers.add(new Answer("b","segunda alternativa"));
@@ -22,51 +26,117 @@ public class TheSurveyManager {
         question2.answers.add(new Answer("d","quarta alternativa"));
         question2.answers.add(new Answer("e","quinta alternativa"));
         
+             
         
+        //o usuário digita o primeiro comando        
         String command = s.nextLine();
+        
+        //essa variável controla se novas respostas estão habilitadas ou não
         boolean allow_new_responses = false;
+        
+        //Interface de comando, enquanto o comando não for 'exit', ela continua rodando
         while(!command.equals("exit")) {
-            if(command.equals("answer")) {
-                if(allow_new_responses) {
-                    ApplySurvey();
-                }
-                else {
-                    System.out.println("Novas respostas á pesquisa estão desativadas");
-                }             
-            }
-            else if(command.equals("check")) {
-                CheckResults();
-            }
-            else if(command.equals("list")) {
-                QuestionsList();
+            switch (command) {
+                
+                //Se o comanando for answer
+                case "answer":
+                    
+                    //se novas respostas estiverem habilitadas, o programa cg=hama o método que aplica a pesquisa 
+                    if(allow_new_responses) {
+                        ApplySurvey();
+                    }
+                    
+                    //senão, é exibida uma mensagem de erro
+                    else {
+                        System.out.println("Novas respostas á pesquisa estão desativadas");
+                    }   break;
+                
+                //Se o comando for 'results', o programa chama o método que mostra os resultados da pesquisa    
+                case "results":
+                    CheckResults();
+                    break;
+                
+                //Se o comando for 'history', o programa chama o método que mostra 
+                //cada pesquisa respondida por cada participante    
+                case "history":
+                    CheckHistory();
+                    break;
+                
+                //Se o comando for 'list', o programa chama o método que mostra todas 
+                //as perguntas registradas no sistema    
+                case "list":
+                    QuestionsList();
+                    break;
+                
+                //Se o comando for 'end', o programa seta o valor da variavel allow_new_responses como falso,
+                //desabilitando que a pesquisa seja respondida   
+                case "end":
+                    allow_new_responses = false;
+                    System.out.println("Novas respostas á pesquisa estão agora desativadas");
+                    break;
+                
+                //Se o comando for 'start', o programa seta o valor da variavel allow_new_responses como true,
+                //habilitando que a pesquisa seja respondida
+                case "start":
+                    allow_new_responses = true;
+                    System.out.println("Novas respostas á pesquisa estão agora ativadas");
+                    break;
             }
             
-            
-            
-            else if(command.equals("end")) {
-                allow_new_responses = false;
-                System.out.println("Novas respostas á pesquisa estão agora desativadas");
-            }
-            else if(command.equals("start")) {
-                allow_new_responses = true;
-                System.out.println("Novas respostas á pesquisa estão agora ativadas");
-            }
-            
-            
+            //O sistema pede um novo comando para o usuário
             command = s.nextLine();
         }
         
     }
     
+    //Método que aplica a pesquisa
     public static void ApplySurvey() {
+        //Cria-se um novo histórico para armazenar as informações da pesquisa
+        History.list.add(new History());
+        
+        //O método que exibe a questão para ser respondida é chamado para
+        //cada item da lista de questões
         Question.list.forEach(n -> n.AnswerQuestion());
     }
     
+    
+    //Método que mostra o histórico de cada pesquisa respondida por cada participante
+    public static void CheckHistory() {
+        
+        //Para cada item da lista de históricos
+        History.list.forEach(history -> {
+            
+            //Para cada item da lista de conteúdos do histórico
+            history.content.forEach(content -> {
+                
+                //O programa obtém a questão através dos dados armazenados no item do histórico
+                Question question = Question.list.get(Integer.parseInt(content[0]));
+                
+                //O programa chama o método que exibe a questão
+                question.ShowQuestion();
+                
+                //O sistema mostra a letra da resposta
+                System.out.println("Resposta -> " + content[1]);
+                
+                //Pula uma linha
+                System.out.println("");
+            });
+            
+            //Após todas as questões de uma pesquisa serem mostradas
+            //deixa uma linha pontilhada para separar da próxima
+            System.out.println("---------------------------------------------");
+        });
+    }
+    
+    //Método que mostra os resultados da pesquisa
     public static void CheckResults() {
+        //Para cada questão da lista, o sistema chama o método que mostra os resultados
         Question.list.forEach(n -> n.getAnswers());
     }
     
+    //Método que mostra todas as questões registradas no sistema
     public static void QuestionsList() {
+        //Para cada questão da lista, o sistema chama o método que mostra a questão
         Question.list.forEach(n -> n.ShowQuestion());
     }
    
