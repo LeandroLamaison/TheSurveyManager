2f51de13ee311ca9a1cba7f77e8475066ea1e2bc
Leandro Lamaison <lamaisonleandro2@gmail.com>
2020-01-06 13:42:58 -0300

    1.1.0 - sem comentários

--- src/thesurveymanager/Alternative.java
+++ src/thesurveymanager/Alternative.java
@@ -3,13 +3,11 @@ package thesurveymanager;
 import java.io.Serializable;
 
 public class Alternative implements Serializable{
-    //Atributos da classe
     private int QUESTION_ID;
     private char ID;
     private String message;
     private int count;
     
-    //Construtor da classe
     Alternative(int QUESTION_ID, char ID, String message) {
         this.QUESTION_ID = QUESTION_ID;
         this.ID = ID;
@@ -17,32 +15,26 @@ public class Alternative implements Serializable{
         this.count = 0;
     }
     
-    //Método de acesso ao id da questão á que a alternativa pertence
     public int getQuestion() {
         return QUESTION_ID;
     }
 
-    //Método de acesso á mensagem da alternativa
     public String getMessage() {
         return message;
     }
     
-    //Método de acesso ao contador de quantas vezes ela foi selecionada
     public int getCount() {
         return count;
     }
     
-    //Método de acesso ao id da alternativa
     public char getID() {
         return ID;
     }
     
-    //Adiciona mais um ao contador
     public void increment() {
         count++;
     }
     
-    //Método que pega a porcentagem 
     public int getPercentage() {
         if(this.count <= 0) {
             return 0;
@@ -52,7 +44,6 @@ public class Alternative implements Serializable{
         }
     }
     
-    //Método que define a mensagem da alternativa
     public void setMessage(String message) {
         this.message = this.ID + ") " + message;
     }
--- src/thesurveymanager/Question.java
+++ src/thesurveymanager/Question.java
@@ -4,46 +4,37 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 public class Question implements Serializable{  
-    //Atributos da classe
     private int id;
     private String message;
     private ArrayList<Alternative> alternatives;
     private int answer_count;
     
-    //Construtor da classe
     Question(String message) {
         this.message = message;
         this.answer_count = 0;
         this.alternatives = new ArrayList<>();
     }
     
-   
-    //Métodos de acesso ao id da questão
     public int getId() {
         return id;
     }
     
-    //Método para definir o id da questão
     public void setId(int id) {
         this.id = id;
     }
     
-    //Método de acesso á mensagem da questão
     public String getMessage() {
         return message;
     }
     
-    //Método de acesso á lista de alternativa
     public ArrayList<Alternative> getAlternatives() {
         return alternatives;
     }
     
-    //Método de acesso ao contador de respostas
     public int getAnswerCount() {
         return answer_count;
     }
 
-    //Método para pegar uma alternativa específica pelo id dela
     public Alternative getAlternative(char id) {
         Alternative alt = null;
         
@@ -56,7 +47,6 @@ public class Question implements Serializable{
         return alt;
     }
     
-    //Método para adicionar resposta
     public void addAnswer(String alternative) {
         answer_count++;
         
@@ -67,10 +57,8 @@ public class Question implements Serializable{
         });
     }
     
-    //Método para cadastrar alternativa
     public void addAlternative(char option, String alternative) {
         alternatives.add(new Alternative(this.id,option,alternative));
     }
-    
     
 }
